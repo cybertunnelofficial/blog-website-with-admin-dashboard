f1b47d4962a59511f2b875a71f3b44ee7848758b
cybertunnelofficial <85891685+cybertunnelofficial@users.noreply.github.com>
2021-06-17 12:26:48 +0530

    Add files via upload

--- blog-single.php
+++ blog-single.php
@@ -15,7 +15,81 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-   <?php include_once("ct-includes/ct-header.php"); ?>
+    
+    <div class="wrap">
+      <header role="banner">
+        <div class="top-bar">
+          <div class="container">
+            <div class="row">
+              <div class="col-9 social">
+                <a href="#"><span class="fa fa-twitter"></span></a>
+                <a href="#"><span class="fa fa-facebook"></span></a>
+                <a href="#"><span class="fa fa-instagram"></span></a>
+                <a href="#"><span class="fa fa-youtube-play"></span></a>
+              </div>
+              <div class="col-3 search-top">
+                <!-- <a href="#"><span class="fa fa-search"></span></a> -->
+                <form action="#" class="search-top-form">
+                  <span class="icon fa fa-search"></span>
+                  <input type="text" id="s" placeholder="Type keyword to search...">
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container logo-wrap">
+          <div class="row pt-5">
+            <div class="col-12 text-center">
+              <a class="absolute-toggle d-block d-md-none" data-toggle="collapse" href="#navbarMenu" role="button" aria-expanded="false" aria-controls="navbarMenu"><span class="burger-lines"></span></a>
+              <h1 class="site-logo"><a href="index.html">Wordify</a></h1>
+            </div>
+          </div>
+        </div>
+        
+        <nav class="navbar navbar-expand-md  navbar-light bg-light">
+          <div class="container">
+            
+            
+            <div class="collapse navbar-collapse" id="navbarMenu">
+              <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Business</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="category.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Travel</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdown04">
+                    <a class="dropdown-item" href="category.html">Asia</a>
+                    <a class="dropdown-item" href="category.html">Europe</a>
+                    <a class="dropdown-item" href="category.html">Dubai</a>
+                    <a class="dropdown-item" href="category.html">Africa</a>
+                    <a class="dropdown-item" href="category.html">South America</a>
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="category.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdown05">
+                    <a class="dropdown-item" href="category.html">Lifestyle</a>
+                    <a class="dropdown-item" href="category.html">Food</a>
+                    <a class="dropdown-item" href="category.html">Adventure</a>
+                    <a class="dropdown-item" href="category.html">Travel</a>
+                    <a class="dropdown-item" href="category.html">Business</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+        </nav>
+      </header>
       <!-- END header -->
       <section class="site-section py-lg">
         <div class="container">
@@ -160,8 +234,105 @@
                 </div>
               </div>
             </div>
-              <?php include_once("ct-includes/ct-sidebar.php"); ?>
-
+            <!-- END main-content -->
+            <div class="col-md-12 col-lg-4 sidebar">
+              <div class="sidebar-box search-form-wrap">
+                <form action="#" class="search-form">
+                  <div class="form-group">
+                    <span class="icon fa fa-search"></span>
+                    <input type="text" class="form-control" id="s" placeholder="Type a keyword and hit enter">
+                  </div>
+                </form>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <div class="bio text-center">
+                  <img src="images/person_2.jpg" alt="Image Placeholder" class="img-fluid">
+                  <div class="bio-body">
+                    <h2>Craig David</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem facilis sunt repellendus excepturi beatae porro debitis voluptate nulla quo veniam fuga sit molestias minus.</p>
+                    <p><a href="#" class="btn btn-primary btn-sm rounded">Read my bio</a></p>
+                    <p class="social">
+                      <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
+                      <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
+                      <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
+                      <a href="#" class="p-2"><span class="fa fa-youtube-play"></span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <h3 class="heading">Popular Posts</h3>
+                <div class="post-entry-sidebar">
+                  <ul>
+                    <li>
+                      <a href="">
+                        <img src="images/img_1.jpg" alt="Image placeholder" class="mr-4">
+                        <div class="text">
+                          <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                          <div class="post-meta">
+                            <span class="mr-2">March 15, 2018 </span>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <img src="images/img_1.jpg" alt="Image placeholder" class="mr-4">
+                        <div class="text">
+                          <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                          <div class="post-meta">
+                            <span class="mr-2">March 15, 2018 </span>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <img src="images/img_1.jpg" alt="Image placeholder" class="mr-4">
+                        <div class="text">
+                          <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                          <div class="post-meta">
+                            <span class="mr-2">March 15, 2018 </span>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <h3 class="heading">Categories</h3>
+                <ul class="categories">
+                  <li><a href="#">Food <span>(12)</span></a></li>
+                  <li><a href="#">Travel <span>(22)</span></a></li>
+                  <li><a href="#">Lifestyle <span>(37)</span></a></li>
+                  <li><a href="#">Business <span>(42)</span></a></li>
+                  <li><a href="#">Adventure <span>(14)</span></a></li>
+                </ul>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <h3 class="heading">Tags</h3>
+                <ul class="tags">
+                  <li><a href="#">Travel</a></li>
+                  <li><a href="#">Adventure</a></li>
+                  <li><a href="#">Food</a></li>
+                  <li><a href="#">Lifestyle</a></li>
+                  <li><a href="#">Business</a></li>
+                  <li><a href="#">Freelancing</a></li>
+                  <li><a href="#">Travel</a></li>
+                  <li><a href="#">Adventure</a></li>
+                  <li><a href="#">Food</a></li>
+                  <li><a href="#">Lifestyle</a></li>
+                  <li><a href="#">Business</a></li>
+                  <li><a href="#">Freelancing</a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- END sidebar -->
           </div>
         </div>
       </section>
@@ -214,7 +385,100 @@
       </section>
       <!-- END section -->
       
-<?php include_once("ct-includes/ct-footer.php"); ?>
+      <footer class="site-footer">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-4">
+              <h3>About Us</h3>
+              <p class="mb-4">
+                <img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid">
+              </p>
+              <p>Lorem ipsum dolor sit amet sa ksal sk sa, consectetur adipisicing elit. Ipsa harum inventore reiciendis. <a href="#">Read More</a></p>
+            </div>
+            <div class="col-md-6 ml-auto">
+              <div class="row">
+                <div class="col-md-7">
+                  <h3>Latest Post</h3>
+                  <div class="post-entry-sidebar">
+                    <ul>
+                      <li>
+                        <a href="">
+                          <img src="images/img_6.jpg" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>How to Find the Video Games of Your Youth</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">March 15, 2018 </span> &bullet;
+                              <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src="images/img_3.jpg" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>How to Find the Video Games of Your Youth</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">March 15, 2018 </span> &bullet;
+                              <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src="images/img_4.jpg" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>How to Find the Video Games of Your Youth</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">March 15, 2018 </span> &bullet;
+                              <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-md-1"></div>
+                
+                <div class="col-md-4">
+                  <div class="mb-5">
+                    <h3>Quick Links</h3>
+                    <ul class="list-unstyled">
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Travel</a></li>
+                      <li><a href="#">Adventure</a></li>
+                      <li><a href="#">Courses</a></li>
+                      <li><a href="#">Categories</a></li>
+                    </ul>
+                  </div>
+                  
+                  <div class="mb-5">
+                    <h3>Social</h3>
+                    <ul class="list-unstyled footer-social">
+                      <li><a href="#"><span class="fa fa-twitter"></span> Twitter</a></li>
+                      <li><a href="#"><span class="fa fa-facebook"></span> Facebook</a></li>
+                      <li><a href="#"><span class="fa fa-instagram"></span> Instagram</a></li>
+                      <li><a href="#"><span class="fa fa-vimeo"></span> Vimeo</a></li>
+                      <li><a href="#"><span class="fa fa-youtube-play"></span> Youtube</a></li>
+                      <li><a href="#"><span class="fa fa-snapchat"></span> Snapshot</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <p class="small">
+                Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> iDense - Theme by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- END footer -->
     </div>
     
     <!-- loader -->
--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php require("ct-config.php"); ?>
-<?php include_once("ct-includes/ct-sitedata.php"); ?>
-<?php include_once("ct-includes/ct-plugins.php"); ?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -91,12 +87,7 @@
           <div class="row blog-entries">
             <div class="col-md-12 col-lg-8 main-content">
               <div class="row">
-                <?php
-                  $postquery = "SELECT * FROM siteposts";
-                  $runquery = mysqli_query($connection, $postquery);
-                  while($posts=mysqli_fetch_assoc($connection, $postquery)){
-                  ?> 
-                    <!-- <div class="col-md-6">
+                <div class="col-md-6">
                   <a href="blog-single.html" class="blog-entry element-animate" data-animate-effect="fadeIn">
                     <img src="images/img_5.jpg" alt="Image placeholder">
                     <div class="blog-content-body">
@@ -108,25 +99,8 @@
                       <h2>How to Find the Video Games of Your Youth</h2>
                     </div>
                   </a>
-                </div> -->
-
-                    <?php
-                  }
-                ?>
-<!--                 <div class="col-md-6">
-                  <a href="blog-single.html" class="blog-entry element-animate" data-animate-effect="fadeIn">
-                    <img src="images/img_5.jpg" alt="Image placeholder">
-                    <div class="blog-content-body">
-                      <div class="post-meta">
-                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
-                        <span class="mr-2">March 15, 2018 </span> &bullet;
-                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                      </div>
-                      <h2>How to Find the Video Games of Your Youth</h2>
-                    </div>
-                  </a>
-                </div> -->
-<!--                 <div class="col-md-6">
+                </div>
+                <div class="col-md-6">
                   <a href="blog-single.html" class="blog-entry element-animate" data-animate-effect="fadeIn">
                     <img src="images/img_6.jpg" alt="Image placeholder">
                     <div class="blog-content-body">
@@ -216,7 +190,7 @@
                       <h2>How to Find the Video Games of Your Youth</h2>
                     </div>
                   </a>
-                </div> -->
+                </div>
               </div>
               <div class="row mt-5">
                 <div class="col-md-12 text-center">
@@ -233,14 +207,207 @@
                   </nav>
                 </div>
               </div>
+              
+              
             </div>
             <!-- END main-content -->
-              <?php include_once("ct-includes/ct-sidebar.php"); ?>
+            <div class="col-md-12 col-lg-4 sidebar">
+              <div class="sidebar-box search-form-wrap">
+                <form action="#" class="search-form">
+                  <div class="form-group">
+                    <span class="icon fa fa-search"></span>
+                    <input type="text" class="form-control" id="s" placeholder="Type a keyword and hit enter">
+                  </div>
+                </form>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <div class="bio text-center">
+                  <img src="images/person_1.jpg" alt="Image Placeholder" class="img-fluid">
+                  <div class="bio-body">
+                    <h2>David Craig</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem facilis sunt repellendus excepturi beatae porro debitis voluptate nulla quo veniam fuga sit molestias minus.</p>
+                    <p><a href="#" class="btn btn-primary btn-sm rounded">Read my bio</a></p>
+                    <p class="social">
+                      <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
+                      <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
+                      <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
+                      <a href="#" class="p-2"><span class="fa fa-youtube-play"></span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <h3 class="heading">Popular Posts</h3>
+                <div class="post-entry-sidebar">
+                  <ul>
+                    <li>
+                      <a href="">
+                        <img src="images/img_2.jpg" alt="Image placeholder" class="mr-4">
+                        <div class="text">
+                          <h4>How to Find the Video Games of Your Youth</h4>
+                          <div class="post-meta">
+                            <span class="mr-2">March 15, 2018 </span>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <img src="images/img_4.jpg" alt="Image placeholder" class="mr-4">
+                        <div class="text">
+                          <h4>How to Find the Video Games of Your Youth</h4>
+                          <div class="post-meta">
+                            <span class="mr-2">March 15, 2018 </span>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <img src="images/img_12.jpg" alt="Image placeholder" class="mr-4">
+                        <div class="text">
+                          <h4>How to Find the Video Games of Your Youth</h4>
+                          <div class="post-meta">
+                            <span class="mr-2">March 15, 2018 </span>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <h3 class="heading">Categories</h3>
+                <ul class="categories">
+                  <li><a href="#">Food <span>(12)</span></a></li>
+                  <li><a href="#">Travel <span>(22)</span></a></li>
+                  <li><a href="#">Lifestyle <span>(37)</span></a></li>
+                  <li><a href="#">Business <span>(42)</span></a></li>
+                  <li><a href="#">Adventure <span>(14)</span></a></li>
+                </ul>
+              </div>
+              <!-- END sidebar-box -->
+              <div class="sidebar-box">
+                <h3 class="heading">Tags</h3>
+                <ul class="tags">
+                  <li><a href="#">Travel</a></li>
+                  <li><a href="#">Adventure</a></li>
+                  <li><a href="#">Food</a></li>
+                  <li><a href="#">Lifestyle</a></li>
+                  <li><a href="#">Business</a></li>
+                  <li><a href="#">Freelancing</a></li>
+                  <li><a href="#">Travel</a></li>
+                  <li><a href="#">Adventure</a></li>
+                  <li><a href="#">Food</a></li>
+                  <li><a href="#">Lifestyle</a></li>
+                  <li><a href="#">Business</a></li>
+                  <li><a href="#">Freelancing</a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- END sidebar -->
           </div>
         </div>
       </section>
+      
+      <footer class="site-footer">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-4">
+              <h3>About Us</h3>
+              <p class="mb-4">
+                <img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid">
+              </p>
+              <p>Lorem ipsum dolor sit amet sa ksal sk sa, consectetur adipisicing elit. Ipsa harum inventore reiciendis. <a href="#">Read More</a></p>
+            </div>
+            <div class="col-md-6 ml-auto">
+              <div class="row">
+                <div class="col-md-7">
+                  <h3>Latest Post</h3>
+                  <div class="post-entry-sidebar">
+                    <ul>
+                      <li>
+                        <a href="">
+                          <img src="images/img_6.jpg" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>How to Find the Video Games of Your Youth</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">March 15, 2018 </span> &bullet;
+                              <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src="images/img_3.jpg" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>How to Find the Video Games of Your Youth</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">March 15, 2018 </span> &bullet;
+                              <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src="images/img_4.jpg" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>How to Find the Video Games of Your Youth</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">March 15, 2018 </span> &bullet;
+                              <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-md-1"></div>
+                
+                <div class="col-md-4">
+                  <div class="mb-5">
+                    <h3>Quick Links</h3>
+                    <ul class="list-unstyled">
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Travel</a></li>
+                      <li><a href="#">Adventure</a></li>
+                      <li><a href="#">Courses</a></li>
+                      <li><a href="#">Categories</a></li>
+                    </ul>
+                  </div>
+                  
+                  <div class="mb-5">
+                    <h3>Social</h3>
+                    <ul class="list-unstyled footer-social">
+                      <li><a href="#"><span class="fa fa-twitter"></span> Twitter</a></li>
+                      <li><a href="#"><span class="fa fa-facebook"></span> Facebook</a></li>
+                      <li><a href="#"><span class="fa fa-instagram"></span> Instagram</a></li>
+                      <li><a href="#"><span class="fa fa-vimeo"></span> Vimeo</a></li>
+                      <li><a href="#"><span class="fa fa-youtube-play"></span> Youtube</a></li>
+                      <li><a href="#"><span class="fa fa-snapchat"></span> Snapshot</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-center">
+<p class="small">
+  Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> iDense - Theme by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- END footer -->
     </div>
-            <?php include_once("ct-includes/ct-footer.php"); ?>
     
     <!-- loader -->
     <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
